6023094d5713d9721562338988ddf19dfe069988
Philip Deuchler <philip.deuchler@sendgrid.com>
2014-11-24 23:39:24 -0700

    Changed difference to an XOR

--- diff.py
+++ diff.py
@@ -5,6 +5,12 @@ def diffy_lube(sets, n):
         "intersection": [],
         "difference": []
         }
-    result["intersection"] = list(sets[0].intersection(*sets[1:]))
-    result["difference"] = list(sets[0].difference(*sets[1:]))
+    intersection = sets[0].intersection(*sets[1:])
+    # The below doesn't work so well, since it depends on which set is first on the list
+    # Plus, we should make this general for n sized comparisons
+    # mathematical_difference = sets[0].difference(*sets[1:])
+    semetric_difference = sets[0].semetric_difference(*sets[1:])
+    result["intersection"] = list(intersection)
+    result["difference"] = list(semetric_difference)
+    # result["mathematical_difference"] = list(mathematical_difference)
     return result
